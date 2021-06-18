367c7ab3bd2394c6082be324d3ed6ced7d9461ad
lucaskrause <lucasggkrause@gmail.com>
2021-06-18 01:59:02 -0300

    Precificacoes OK

--- src/components/pages/precificacoes/Edit.vue
+++ src/components/pages/precificacoes/Edit.vue
@@ -1,31 +1,26 @@
 <template>
     <div class="col-12">
-        <h2 v-if="!isModal">Cadastro de Preço</h2>
-        <hr v-if="!isModal"/>
+        <h2>Cadastro de Preço</h2>
+        <hr/>
         <div class="row form-group">
             <div class="col-1">
                 <label>Código</label>
-                <input id="codigo" type="number" class="form-control" v-model="entity.codigo" readonly/>
+                <input id="codigo" type="number" class="form-control" v-model.number="entity.codigo" readonly/>
+            </div>
+
+            <div class="col-2">
+                <label>Mín de Pessoas</label>
+                <input id="minPessoas" type="number" class="form-control" v-model.number="entity.minPessoas"/>
+            </div>
+
+            <div class="col-2">
+                <label>Máx de Pessoas</label>
+                <input id="maxPessoas" type="number" class="form-control" v-model.number="entity.maxPessoas"/>
             </div>
 
             <div class="col-2">
                 <label>Valor</label>
-                <input id="valor" type="number" class="form-control" v-model="entity.valor"/>
-            </div>
-
-            <div class="col-3">
-                <label>Quantidade de Pessoas</label>
-                <input id="sigla" type="number" class="form-control" v-model="entity.qtdePessoas"/>
-            </div>
-
-            <div class="col-3">
-                <label>Tipo</label>
-                <select id="tipo" class="form-control" v-model="entity.tipo">
-                    <option selected>Selecione...</option>
-                    <option value="Campo">Campo</option>
-                    <option value="Sede Social">Sede Social</option>
-                    <option value="Sede Social e Campo">Sede Social e Campo</option>
-                </select>
+                <input id="valor" type="number" class="form-control" v-model.number="entity.valor"/>
             </div>
         </div>
 
@@ -42,7 +37,7 @@
 
             <div class="col-8">
                 <div class="text-right">
-                    <router-link v-if="!isModal" :to="{name: 'PrecificacoesList'}" class="btn btn-danger mr-3">Voltar</router-link>
+                    <router-link :to="{name: 'PrecificacoesList'}" class="btn btn-danger mr-3">Voltar</router-link>
                     <input type="submit" value="Salvar" class="btn btn-success" @click.prevent="save()" :class="{'disabled': isSubmiting}">
                 </div>
             </div>
@@ -59,21 +54,15 @@ const notyf = new Notyf();
 
 export default {
     name: "PrecificacoesEdit",
-    props: {
-        isModal: {
-            type: Boolean,
-            default: false
-        }
-    },
     data() {
         return {
             entity: {
                 codigo: 0,
-                valor: "",
-                qtdePessoas: "",
-                tipo: "",
-                dtCadastro: "",
-                dtAlteracao: ""
+                minPessoas: null,
+                maxPessoas: null,
+                valor: null,
+                dtCadastro: null,
+                dtAlteracao: null
             },
             isSubmiting: false
         }
@@ -82,8 +71,7 @@ export default {
         const vm = this;
         if (this.$route.params.codigo) {
             this.entity.codigo = this.$route.params.codigo;
-        }
-        if (this.entity.codigo) {
+
             PrecificacoesService.getById(this.entity.codigo).then(function (response) {
                 vm.entity = response.data;
             });
@@ -93,19 +81,12 @@ export default {
         save() {
             if (this.isSubmiting) return;
             this.isSubmiting = true;
-            this.$delete(this.entity, 'dtCadastro');
-            this.$delete(this.entity, 'dtAlteracao');
             const vm = this;
-            PrecificacoesService.save(this.entity).then(function (response) {
+            PrecificacoesService.save(this.entity).then(function () {
                 const msg = vm.entity.codigo ? "editado" : 'criado';
                 notyf.success("Preço " + msg + " com sucesso");
                 vm.isSubmiting = false;
-                if(vm.isModal){
-                    vm.entity.codigo = response.data.codigo;
-                    vm.$emit('emit-preco', vm.entity);
-                } else {
-                    vm.$router.push('/app/precificacoes');
-                }
+                vm.$router.push('/app/precificacoes');
             }); // .catch((errors) => Helper.saveErrorCallBack(errors.response));
         }
     }
