aba1fd0b0e5231afe16688d1c7eba0244428bcb3
lucaskrause <lucasggkrause@gmail.com>
2021-07-09 22:26:46 -0300

    add validação de idade

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -49,6 +49,30 @@ const Helper = {
             }
         })
     },
+    
+    calculaIdade(dataNasc) { 
+        var dataAtual = new Date();
+        var anoAtual = dataAtual.getFullYear();
+        var anoNascParts = dataNasc.split('-');
+        var diaNasc = anoNascParts[2];
+        var mesNasc = anoNascParts[1];
+        var anoNasc = anoNascParts[0];
+        var idade = anoAtual - anoNasc;
+        var mesAtual = dataAtual.getMonth() + 1; 
+        //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
+        if(mesAtual < mesNasc){
+            idade--; 
+        } else {
+            //Se estiver no mes do nascimento, verificar o dia
+            if(mesAtual == mesNasc){ 
+                if(new Date().getDate() < diaNasc ){ 
+                    //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
+                    idade--; 
+                }
+            }
+        } 
+        return idade; 
+    },
 
     validadorEmail(email) {
         var usuario = email.substring(0, email.indexOf("@"));
