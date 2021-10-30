696763610e6b3536202a9f0e0bfefabcc42bbaeb
lucaskrause <lucasggkrause@gmail.com>
2021-10-29 22:37:15 -0300

    validation agendamento

--- src/components/pages/agendamento/Agenda.vue
+++ src/components/pages/agendamento/Agenda.vue
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import {AreasLocacaoService} from '@/services/areasLocacao.service'
 import {LocacoesService} from '@/services/locacoes.service'
 import Helper from '@/components/helper'
 import PopoverRow from 'v-calendar/lib/components/popover-row.umd.min'
@@ -40,8 +41,9 @@ export default {
     components: { PopoverRow },
     data() {
         return {
-            colors: ['red', 'blue', 'brown', 'black'],
-            minData: Helper.addDays(new Date(), 4),
+            ListaAreasLocacao: [],
+            colors: ['red', 'blue', 'green', 'orange', 'yellow'],
+            minData: Helper.addDays(new Date(), 3),
             disabledDates: [],
             masks: { weekdays: 'WWWW' },
             attributes: [],
@@ -50,33 +52,58 @@ export default {
     },
     created() {
         const vm = this;
-        LocacoesService.getAll().then(function (response) {
-            var dateLocacao;
-            var attribute;
-            var disabledDate;
-            var locacao;
-            var area;
-            for (let i = 0; i < response.data.length; i++) {
-                locacao = response.data[i];
-                dateLocacao = Helper.addDays(Helper.dateToDateString(locacao.dtLocacao), 1);
-                for (let j = 0; j < locacao.areasLocacao.length; j++) {
-                    area = locacao.areasLocacao[j];
-                    attribute = {
-                        key: i + 1,
-                        bar: vm.colors[j],
-                        popover: {
-                            label: area.descricao
-                        },
-                        dates: dateLocacao
+
+        AreasLocacaoService.getAll().then(function (response) {
+            vm.ListaAreasLocacao = response.data.map((area) => area.descricao);
+
+            LocacoesService.getAll().then(function (response) {
+                var locacoes = response.data;
+                var locacao;
+                var area;
+                var dateLocacao;
+                var attribute;
+                var validation = [];
+
+                for (let i = 0; i < locacoes.length; i++) {
+                    locacao = locacoes[i];
+                    dateLocacao = Helper.dateToDateString(Helper.addDays(locacao.dtLocacao, 1));
+
+                    for (let j = 0; j < locacao.areasLocacao.length; j++) {
+                        area = locacao.areasLocacao[j];
+                        attribute = {
+                            key: i + 1,
+                            bar: vm.colors[Math.floor(Math.random()*vm.colors.length)],
+                            popover: {
+                                label: area.descricao
+                            },
+                            dates: dateLocacao
+                        }
+                        vm.attributes.push(attribute);
+                        
+                        if (!validation[dateLocacao]) {
+                            validation[dateLocacao] = [];
+                        }
+                        validation[dateLocacao].push(area.descricao);
                     }
-                    vm.attributes.push(attribute);
                 }
-                disabledDate = {
-                    start: dateLocacao,
-                    end: dateLocacao 
-                };
-                vm.disabledDates.push(disabledDate);
-            }
+                
+                var dates = Object.keys(validation);
+                var dayLocation;
+
+                for (let k = 0; k < dates.length; k++) {
+                    dayLocation = validation[dates[k]];
+
+                    var disabled = true;
+                    for (let l = 0; l < vm.ListaAreasLocacao.length; l++) {
+                        disabled = dayLocation.includes(vm.ListaAreasLocacao[l]);
+                        if (!disabled) break;
+                    }
+                    
+                    if (disabled) {
+                        vm.disabledDates.push( {start: dates[k], end: dates[k]} );
+                    }
+                }
+            });
         });
     },
     methods: {
