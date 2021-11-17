c6883adcd0b87f559d91ab6ab4d5c575e42a74eb
lucaskrause <lucasggkrause@gmail.com>
2021-11-16 22:46:12 -0300

    Add mascara CPF

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -164,6 +164,10 @@ const Helper = {
         return true;
     },
 
+    addMaskCPF(cpf) {
+        return cpf.replace(/\D/g, '').replace(/^(\d{3})?(\d{3})?(\d{3})?(\d{2})?/, "$1.$2.$3-$4");
+    },
+
     addMaskCNPJ(cnpj) {
         return cnpj.replace(/\D/g, '').replace(/^(\d{2})(\d{3})?(\d{3})?(\d{4})?(\d{2})?/, "$1.$2.$3/$4-$5");
     },
