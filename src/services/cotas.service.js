7cddb72b4788349d4263d8173b4eb400472605e3
lucaskrause <lucasggkrause@gmail.com>
2021-11-18 00:05:19 -0300

    parcelas de cotista

--- src/services/cotas.service.js
+++ src/services/cotas.service.js
@@ -1,5 +1,9 @@
 import {Service} from "@/services/service";
 
-const instance = new class CotasService extends Service{};
+const instance = new class CotasService extends Service{
+    gerarParcelas(entity) {
+        return this.api.post(this.endpoint + "/parcelas", entity);
+    }
+};
 
 export {instance as CotasService}
